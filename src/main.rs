1150faf1f0cd0c35ec7459be38e7abfc4a6e01c4
aagbotemi <aabiodunawoyemi@gamil.com>
2024-03-12 23:05:30 +0100

    changed public key to hex for better UI

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,29 @@ use lambdaworks_math::{
         },
         traits::IsEllipticCurve,
     },
+    traits::{AsBytes, ByteConversion},
     unsigned_integer::element::{UnsignedInteger, U256},
 };
 
 pub struct SparklingWaterPK {}
 
 pub trait SparklingWaterPKTrait {
-    fn generate_public_key(secret_key: &str)
-        -> ShortWeierstrassProjectivePoint<BLS12381Curve>;
+    fn generate_public_key(secret_key: &str) -> ShortWeierstrassProjectivePoint<BLS12381Curve>;
 }
 
 impl SparklingWaterPKTrait for SparklingWaterPK {
-    fn generate_public_key(
-        secret_key: &str,
-    ) -> ShortWeierstrassProjectivePoint<BLS12381Curve> {
+    fn generate_public_key(secret_key: &str) -> ShortWeierstrassProjectivePoint<BLS12381Curve> {
         let secret_key: UnsignedInteger<4> = U256::from_hex(secret_key).unwrap();
-        
+
         let public_key: ShortWeierstrassProjectivePoint<BLS12381Curve> =
             BLS12381Curve::generator().operate_with_self(secret_key);
 
-        public_key.to_affine()
+        public_key
     }
 }
 
 fn main() {
-    let public_key = SparklingWaterPK::generate_public_key("0x6C616D6264617370");
-    println!("public_key={:?}", public_key);
+    let public_key_bytes = SparklingWaterPK::generate_public_key("0x6C616D6264617370").as_bytes();
+    let public_hex = U256::from_bytes_be(&public_key_bytes).unwrap().to_hex();
+    println!("public_key={}", public_hex);
 }
