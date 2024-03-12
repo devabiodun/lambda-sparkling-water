26d5dcc5fbcb509e7321474473ea6e5154c7e2ea
aagbotemi <aabiodunawoyemi@gamil.com>
2024-03-12 22:14:37 +0100

    changed BLS12381TwistCurve to BLS12381Curve

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use lambdaworks_math::{
     cyclic_group::IsGroup,
     elliptic_curve::{
         short_weierstrass::{
-            curves::bls12_381::twist::BLS12381TwistCurve, point::ShortWeierstrassProjectivePoint,
+            curves::bls12_381::curve::BLS12381Curve, point::ShortWeierstrassProjectivePoint,
         },
         traits::IsEllipticCurve,
     },
@@ -13,16 +13,17 @@ pub struct SparklingWaterPK {}
 
 pub trait SparklingWaterPKTrait {
     fn generate_public_key(secret_key: &str)
-        -> ShortWeierstrassProjectivePoint<BLS12381TwistCurve>;
+        -> ShortWeierstrassProjectivePoint<BLS12381Curve>;
 }
 
 impl SparklingWaterPKTrait for SparklingWaterPK {
     fn generate_public_key(
         secret_key: &str,
-    ) -> ShortWeierstrassProjectivePoint<BLS12381TwistCurve> {
+    ) -> ShortWeierstrassProjectivePoint<BLS12381Curve> {
         let secret_key: UnsignedInteger<4> = U256::from_hex(secret_key).unwrap();
-        let public_key: ShortWeierstrassProjectivePoint<BLS12381TwistCurve> =
-            BLS12381TwistCurve::generator().operate_with_self(secret_key);
+        
+        let public_key: ShortWeierstrassProjectivePoint<BLS12381Curve> =
+            BLS12381Curve::generator().operate_with_self(secret_key);
 
         public_key.to_affine()
     }
